4a3d1059ecd5d0faa148c8e05e4912afb3190040
Nick Romano <dev@njromano.com>
2019-09-10 19:52:09 -0400

    initial commit

---
+++ main.lua
@@ -0,0 +1,20 @@
+local GOON_STRING = "G{skull}{skull}NS";
+local GOON_DELAY = 60;
+
+nextGoonTime = time();
+local playerName = UnitName("player");
+local goonFrame = CreateFrame("Frame");
+goonFrame:RegisterEvent("CHAT_MSG_WHISPER");
+goonFrame:SetScript('OnEvent', function(self, event, msg, sender, ...)
+    local match = string.gmatch(sender, "(*)-*");
+    local nameOnly = "noname";
+    for match in string.gmatch(sender, "(.+)-.") do
+        nameOnly = match
+    end
+    if msg == GOON_STRING and time() > nextGoonTime then
+        if (nameOnly ~= playerName) then
+            SendChatMessage(GOON_STRING, "GUILD");
+        end
+        nextGoonTime = time() +  GOON_DELAY;
+    end
+end)
