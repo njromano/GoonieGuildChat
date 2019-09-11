1225d3a76ed02840e092d4b37bbf03e8afd1df2d
Nick Romano <dev@njromano.com>
2019-09-10 20:28:54 -0400

    now gooning on more goons

--- main.lua
+++ main.lua
@@ -1,20 +1,30 @@
 local GOON_STRING = "G{skull}{skull}NS";
 local GOON_DELAY = 60;
 
+-- set initial goon time
 nextGoonTime = time();
+
+-- get player name and UI frame
 local playerName = UnitName("player");
 local goonFrame = CreateFrame("Frame");
 goonFrame:RegisterEvent("CHAT_MSG_GUILD");
+
+-- script
 goonFrame:SetScript('OnEvent', function(self, event, msg, sender, ...)
+    -- rip out server from 'name-server', leaving only 'name'
     local match = string.gmatch(sender, "(*)-*");
     local nameOnly = "noname";
     for match in string.gmatch(sender, "(.+)-.") do
         nameOnly = match
     end
-    if msg == GOON_STRING and time() > nextGoonTime then
+
+    -- check if we gooned in this message
+    -- remove all spaces
+    msg = string.gsub(msg, "%s", "");
+    if string.find(msg, GOON_STRING) ~= nil and time() > nextGoonTime then
         if (nameOnly ~= playerName) then
             SendChatMessage(GOON_STRING, "GUILD");
         end
-        nextGoonTime = time() +  GOON_DELAY;
+         nextGoonTime = time() +  GOON_DELAY;
     end
 end)
